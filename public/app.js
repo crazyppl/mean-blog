00330fd1d85ab5bdb9c324ba455ca814bc773e5c
nadine <nadinefirdaus@gmail.com>
2017-11-14 23:59:17 +0600

    First Commit

---
+++ public/app.js
@@ -0,0 +1,14 @@
+(function () {
+    angular
+        .module("BlogApp", [])
+        .controller("BlogController", BlogController);
+    
+    function BlogController($scope) {
+        $scope.createPost = createPost;
+        
+        function createPost() {
+            console.log("createPost");
+        }
+
+    }
+})();
